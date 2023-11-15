ce01f231258986aefa25f9bc6e253bff93bffff7
Ian Frankel <ian.frankel@gmail.com>
2023-11-15 15:53:56 -0500

    parasocial relationships

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,31 @@
 class Pet:
-    pass
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+
+    all = []
+
+    def __init__(self, name, pet_type, owner = None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        if self.pet_type not in Pet.PET_TYPES:
+            raise Exception('Not a valid pet type')
+        Pet.all.append(self)
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner.name == self.name]
+    
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            pet.owner = self
+        else:
+            raise Exception(f"{pet} must be a pet")
+        
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key = lambda pet: pet.name)
+
+    
